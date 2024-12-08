8fd16f2674ce176220069c094e0d789c6e82c737
Naman Jain <jnaman475@gmail.com>
2024-12-08 14:35:36 +0530

    Added carots svg and more data to json

--- src/components/CollegeTable.js
+++ src/components/CollegeTable.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./CollegeTable.css";
 import { FaArrowRightArrowLeft, FaArrowRightLong } from "react-icons/fa6";
 import { IoMdDownload } from "react-icons/io";
-import { PiCaretDownBold, PiCaretRightBold, PiCheckBold } from "react-icons/pi";
+import { PiCaretDownBold, PiCheckBold } from "react-icons/pi";
 
 const CollegeTable = () => {
   const [colleges, setColleges] = useState([]);
@@ -10,7 +10,6 @@ const CollegeTable = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [sortConfig, setSortConfig] = useState({ key: "", direction: "" });
   const [loading, setLoading] = useState(false);
-  const [expandedRows, setExpandedRows] = useState([]);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/college.json`)
@@ -62,20 +61,23 @@ const CollegeTable = () => {
     setFilteredColleges(sortedData);
   };
 
-  const toggleRowExpansion = (index) => {
-    setExpandedRows((prev) =>
-      prev.includes(index)
-        ? prev.filter((rowIndex) => rowIndex !== index)
-        : [...prev, index]
-    );
-  };
-
   useEffect(() => {
     const filtered = colleges.filter((college) =>
       college.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setFilteredColleges(filtered.slice(0, 10));
   }, [searchTerm, colleges]);
+
+  const renderCaret = (key) => {
+    if (sortConfig.key === key) {
+      return sortConfig.direction === "asc" ? (
+        <PiCaretDownBold />
+      ) : (
+        <PiCaretDownBold style={{ transform: "rotate(180deg)" }} />
+      );
+    }
+    return <PiCaretDownBold />;
+  };
 
   return (
     <div className="college-table-container">
@@ -91,165 +93,160 @@ const CollegeTable = () => {
         <thead>
           <tr>
             <th onClick={() => sortBy("rank")}>CD Rank</th>
-            <th onClick={() => sortBy("name")}>Colleges</th>
-            <th onClick={() => sortBy("fee")}>Course Fees</th>
-            <th onClick={() => sortBy("placement")}>Placement</th>
-            <th onClick={() => sortBy("userReview")}>User Review</th>
-            <th onClick={() => sortBy("ranking")}>Ranking</th>
+            <th>Colleges</th>
+            <th onClick={() => sortBy("fee")}>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "5px" }}
+              >
+                Course Fees {renderCaret("fee")}
+              </div>
+            </th>
+            <th onClick={() => sortBy("placement")}>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "5px" }}
+              >
+                Placement {renderCaret("placement")}
+              </div>
+            </th>
+            <th onClick={() => sortBy("userReview")}>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "5px" }}
+              >
+                User Review {renderCaret("userReview")}
+              </div>
+            </th>
+            <th>
+              <div>Ranking</div>
+            </th>
           </tr>
         </thead>
         <tbody>
           {filteredColleges.map((college, index) => (
-            <React.Fragment key={index}>
-              <tr
-                style={{
-                  backgroundColor: college.featured === "true" ? "" : "#ffffff",
-                }}
-                className={college.featured ? "featured" : ""}
-              >
-                <td>{college.rank}</td>
-                <td>
-                  <div className="college-info">
-                    {college.featured === "true" && (
-                      <div className="badge">Featured</div>
-                    )}
-
-                    <img
-                      src={college.logo}
-                      alt={college.name}
-                      className="college-logo"
-                    />
-                    <div className="college-details">
-                      <h3>{college.name}</h3>
-                      <p>{college.location}</p>
-                      <div className="college-course-details">
-                        <p
-                          className="course"
-                          onClick={() => toggleRowExpansion(index)}
-                        >
-                          {college.course}{" "}
-                          {expandedRows.includes(index) ? (
-                            <PiCaretDownBold />
-                          ) : (
-                            <PiCaretRightBold />
-                          )}
-                        </p>
-                        <p className="cutoff">{college.cutoff}</p>
-                      </div>
-                    </div>
-                  </div>
-                  {expandedRows.includes(index) && (
-                    <div className="college-btn">
-                      <button className="apply-now">
-                        <FaArrowRightLong /> Apply Now
-                      </button>
-                      <button className="download-brochure">
-                        <IoMdDownload /> Download Brochure
-                      </button>
-                      <button className="add-to-compare">
-                        <input type="checkbox" /> Add to Compare
-                      </button>
-                    </div>
+            <tr
+              style={{
+                backgroundColor: college.featured === "true" ? "" : "#ffffff",
+              }}
+              key={index}
+              className={college.featured ? "featured" : ""}
+            >
+              <td>{college.rank}</td>
+              <td>
+                <div className="college-info">
+                  {college.featured === "true" && (
+                    <div className="badge">Featured</div>
                   )}
-                </td>
-                <td>
-                  <div className="college-fees">
-                    <h3>{college.fee}</h3>
-                    <p>BE/B.Tech</p>
-                    <p>-1st Year Fees</p>
-                    {expandedRows.includes(index) && (
-                      <a href="#">
-                        <FaArrowRightArrowLeft /> Compare Fees
-                      </a>
-                    )}
-                  </div>
-                </td>
-                <td>
-                  <div className="college-fees">
-                    <h3>{college.placement.avgPackage}</h3>
-                    <p>Average Package</p>
-                    <h3>{college.placement.highestPackage}</h3>
-                    <p>Highest Package</p>
 
-                    {expandedRows.includes(index) && (
-                      <a href="#">
-                        <FaArrowRightArrowLeft /> Compare Placement
-                      </a>
-                    )}
-                  </div>
-                </td>
-                <td>
-                  <div className="college-user-rating">
-                    <h3>
-                      <span
-                        style={{
-                          display: "inline-block",
-                          backgroundColor: "orange",
-                          borderRadius: "50%",
-                          width: "6px",
-                          height: "6px",
-                          marginRight: "5px",
-                        }}
-                      ></span>
-
-                      {college.userReview.rating}
-                    </h3>
-                    <p>{college.userReview.base}</p>
-                    <p>Reviews</p>
-                    <p>
-                      <PiCheckBold />
-                      {college.badge}{" "}
-                      {expandedRows.includes(index) ? (
-                        <PiCaretDownBold />
-                      ) : (
-                        <PiCaretRightBold />
-                      )}
-                    </p>
-                  </div>
-                </td>
-
-                <td>
-                  <div className="ranking-section">
-                    <div className="rank-summary">
-                      <span className="rank-position">
-                        {college.ranking.position}
-                      </span>
-                      <span className="rank-details">
-                        /{college.ranking.total}
-                      </span>
-                      <span> in India</span>
+                  <img
+                    src={college.logo}
+                    alt={college.name}
+                    className="college-logo"
+                  />
+                  <div className="college-details">
+                    <h3>{college.name}</h3>
+                    <p>{college.location}</p>
+                    <div className="college-course-details">
+                      <p className="course">
+                        {college.course} <PiCaretDownBold />
+                      </p>
+                      <p className="cutoff">{college.cutoff}</p>
                     </div>
-                    <div className="rank-year">
+                  </div>
+                </div>
+                <div className="college-btn">
+                  <button className="apply-now">
+                    <FaArrowRightLong /> Apply Now
+                  </button>
+                  <button className="download-brochure">
+                    <IoMdDownload /> Download Brochure
+                  </button>
+                  <button className="add-to-compare">
+                    <input type="checkbox" /> Add to Compare
+                  </button>
+                </div>
+              </td>
+              <td>
+                <div className="college-fees">
+                  <h3>{college.fee}</h3>
+                  <p>BE/B.Tech</p>
+                  <p>-1st Year Fees</p>
+                  <a href="#">
+                    <FaArrowRightArrowLeft /> Compare Fees
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="college-fees">
+                  <h3>{college.placement.avgPackage}</h3>
+                  <p>Average Package</p>
+                  <h3>{college.placement.highestPackage}</h3>
+                  <p>Highest Package</p>
+                  <a href="#">
+                    <FaArrowRightArrowLeft /> Compare Placement
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="college-user-rating">
+                  <h3>
+                    <span
+                      style={{
+                        display: "inline-block",
+                        backgroundColor: "orange",
+                        borderRadius: "50%",
+                        width: "6px",
+                        height: "6px",
+                        marginRight: "5px",
+                      }}
+                    ></span>
+
+                    {college.userReview.rating}
+                  </h3>
+                  <p>{college.userReview.base}</p>
+                  <p>Reviews</p>
+                  <p>
+                    <PiCheckBold />
+                    {college.badge} <PiCaretDownBold />
+                  </p>
+                </div>
+              </td>
+
+              <td>
+                <div className="ranking-section">
+                  <div className="rank-summary">
+                    <span className="rank-position">
+                      {college.ranking.position}
+                    </span>
+                    <span className="rank-details">
+                      /{college.ranking.total}
+                    </span>
+                    <span> in India</span>
+                  </div>
+                  <div className="rank-year">
+                    <img
+                      src={college.ranking.logos[1].src}
+                      alt="news"
+                      style={{ width: "56px", height: "auto" }}
+                    />
+
+                    {college.ranking.year}
+                  </div>
+                  <div className="rank-logos">
+                    {college.ranking.logos.slice(0, 3).map((logo, index) => (
                       <img
-                        src={college.ranking.logos[1].src}
-                        alt="news"
-                        style={{ width: "56px", height: "auto" }}
+                        key={index}
+                        src={logo.src}
+                        alt={logo.alt}
+                        className="rank-logo"
                       />
-
-                      {college.ranking.year}
-                    </div>
-                    <div className="rank-logos">
-                      {college.ranking.logos.slice(0, 3).map((logo, index) => (
-                        <img
-                          key={index}
-                          src={logo.src}
-                          alt={logo.alt}
-                          className="rank-logo"
-                        />
-                      ))}
-                      <div className="more-rankings">
-                        <span>+ {college.ranking.additionalRankings} More</span>
-                        {expandedRows.includes(index) ? (
-                          <PiCaretDownBold />
-                        ) : (
-                          <PiCaretRightBold />
-                        )}
-                      </div>
+                    ))}
+                    <div className="more-rankings">
+                      <span>+ {college.ranking.additionalRankings} More</span>
+                      <PiCaretDownBold />
                     </div>
                   </div>
-                </td>
-              </tr>
-            </React.Fragment>
+                </div>
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
